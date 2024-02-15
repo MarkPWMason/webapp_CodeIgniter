82d506b7713ec509e9e6db4430b1d9748e7665e6
Mark Mason <markpwmason@outlook.com>
2024-02-15 11:06:19 +0000

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('articles', 'Articles::index');
+$routes->get('articles/(:num)','Articles::show/$1');
+$routes->get('articles/new','Articles::new');
+$routes->post('articles/create','Articles::create');
